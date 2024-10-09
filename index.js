4c68bb3536cdf0a509187ed5cbed9449d6b31dca
Johne04 <johne1708@gmail.com>
2024-10-09 14:27:17 +0100

    Layout change

--- index.js
+++ index.js
@@ -26,49 +26,25 @@ $(document).ready(function () {
   });
 });
 
-let nextBtn = document.querySelector(".next");
-let prevBtn = document.querySelector(".prev");
+const galleries = document.querySelectorAll('.gallery');
+const lists = document.getElementById('lists');
+const leftBtn = document.getElementById('leftBtn');
+const rightBtn = document.getElementById('rightBtn');
 
-let slider = document.querySelector(".slider");
-let sliderList = slider.querySelector(".slider .list");
-let thumbnail = document.querySelector(".slider .thumbnail");
-let thumbnailItems = thumbnail.querySelectorAll(".item");
+let currentIndex = 0;
 
-thumbnail.appendChild(thumbnailItems[0]);
-
-// Function for next button
-nextBtn.onclick = function () {
-  moveSlider("next");
-};
-
-// Function for prev button
-prevBtn.onclick = function () {
-  moveSlider("prev");
-};
-
-function moveSlider(direction) {
-  let sliderItems = sliderList.querySelectorAll(".item");
-  let thumbnailItems = document.querySelectorAll(".thumbnail .item");
-
-  if (direction === "next") {
-    sliderList.appendChild(sliderItems[0]);
-    thumbnail.appendChild(thumbnailItems[0]);
-    slider.classList.add("next");
-  } else {
-    sliderList.prepend(sliderItems[sliderItems.length - 1]);
-    thumbnail.prepend(thumbnailItems[thumbnailItems.length - 1]);
-    slider.classList.add("prev");
-  }
-
-  slider.addEventListener(
-    "animationend",
-    function () {
-      if (direction === "next") {
-        slider.classList.remove("next");
-      } else {
-        slider.classList.remove("prev");
-      }
-    },
-    { once: true }
-  ); // Remove the event listener after it's triggered once
+function updateSlider() {
+  lists.style.transform = `translateX(-${currentIndex * 100}%)`;
 }
+
+leftBtn.addEventListener('click', () => {
+  currentIndex = (currentIndex === 0) ? galleries.length - 1 : currentIndex - 1;
+  updateSlider();
+});
+
+rightBtn.addEventListener('click', () => {
+  currentIndex = (currentIndex === galleries.length - 1) ? 0 : currentIndex + 1;
+  updateSlider();
+});
+
+updateSlider();
